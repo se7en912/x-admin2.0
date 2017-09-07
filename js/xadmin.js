6d71c9c22d5e27457bc36065168b36aef896b818
zhibinm <113664000@qq.com>
2017-09-07 17:41:37 +0800

    fix

--- js/xadmin.js
+++ js/xadmin.js
@@ -3,137 +3,128 @@ $(function () {
     layui.use(['form','element'],
     function() {
         layer = layui.layer;
-        element = layui.element();
+        element = layui.element;
     });
 
-	//初如化背景
-	function bgint () {
-    	if(localStorage.bglist){
-            var arr = JSON.parse(localStorage.bglist);// 
-            // console.log(arr);
-            //全局背景统一
-            if(arr['bgSrc']){
-                $('body').css('background-image', 'url('+arr['bgSrc']+')');
+    //触发事件
+  var tab = {
+        tabAdd: function(title,url,id){
+          //新增一个Tab项
+          element.tabAdd('xbs_tab', {
+            title: title 
+            ,content: '<iframe tab-id="'+id+'" frameborder="0" src="'+url+'" scrolling="yes" class="x-iframe"></iframe>'
+            ,id: id
+          })
+        }
+        ,tabDelete: function(othis){
+          //删除指定Tab项
+          element.tabDelete('xbs_tab', '44'); //删除：“商品管理”
+          
+          
+          othis.addClass('layui-btn-disabled');
+        }
+        ,tabChange: function(id){
+          //切换到指定Tab项
+          element.tabChange('xbs_tab', id); //切换到：用户管理
+        }
+      };
 
-                //初始化弹出层背景
-            }
-            //单个背景逻辑
-            // if(arr[location.href]){
-            //     $('body').css('background-image', 'url('+arr[location.href]+')');
-            // }
-    	}
+
+    tableCheck = {
+        init:function  () {
+            $(".layui-form-checkbox").click(function(event) {
+                if($(this).hasClass('layui-form-checked')){
+                    $(this).removeClass('layui-form-checked');
+                    if($(this).hasClass('header')){
+                        $(".layui-form-checkbox").removeClass('layui-form-checked');
+                    }
+                }else{
+                    $(this).addClass('layui-form-checked');
+                    if($(this).hasClass('header')){
+                        $(".layui-form-checkbox").addClass('layui-form-checked');
+                    }
+                }
+                
+            });
+        },
+        getData:function  () {
+            var obj = $(".layui-form-checked").not('.header');
+            var arr=[];
+            obj.each(function(index, el) {
+                arr.push(obj.eq(index).attr('data-id'));
+            });
+            return arr;
+        }
     }
 
-    bgint();
+    //开启表格多选
+    tableCheck.init();
+      
 
-	//背景主题功能
-	var changerlist = new Swiper('.changer-list', {
-		initialSlide :5,
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        coverflow: {
-            rotate: 50,
-            stretch: -10,
-            depth: 100,
-            modifier: 1,
-            slideShadows : false
+    $('.container .left_open i').click(function(event) {
+        if($('.left-nav').css('left')=='0px'){
+            $('.left-nav').animate({left: '-221px'}, 100);
+            $('.page-content').animate({left: '0px'}, 100);
+            $('.page-content-bg').hide();
+        }else{
+            $('.left-nav').animate({left: '0px'}, 100);
+            $('.page-content').animate({left: '221px'}, 100);
+            if($(window).width()<768){
+                $('.page-content-bg').show();
+            }
         }
-    });
-
-    //判断是否显示左侧菜单
-    $(window).resize(function(){
-        width=$(this).width();
-        if(width>1024){
-            $('#side-nav').show();
-        }
-    });
-
-	//背景主题设置展示
-    is_show_change=true;
-    $('#changer-set').click(function(event) {
-    	if(is_show_change){
-            $('.bg-out').show();
-    		$('.bg-changer').animate({top: '0px'}, 500);
-    		is_show_change=false;
-    	}else{
-    		$('.bg-changer').animate({top: '-110px'}, 500);
-    		is_show_change=true;
-    	}
-    	
-    });
-
-    //背景主题切换
-    $('.bg-changer img.item').click(function(event) {
-    	if(!localStorage.bglist){
-    		arr = {};
-    	}else{
-    		arr = JSON.parse(localStorage.bglist);
-    	}
-    	var src = $(this).attr('src');
-    	$('body').css('background-image', 'url('+src+')');
-
-    	url = location.href;
-
-        // 单个背景逻辑
-    	// arr[url]=src;
-        
-        // 全局背景统一
-        arr['bgSrc'] = src;
-        // console.log(arr);
-
-    	localStorage.bglist = JSON.stringify(arr);
 
     });
 
-    //背景初始化
-    $('.reset').click(function  () {
-        localStorage.clear();
-        layer.msg('初如化成功',function(){});
-    });
-
-
-    //背景切换点击空白区域隐藏
-    $('.bg-out').click(function  () {
-        $('.bg-changer').animate({top: '-110px'}, 500);
-            is_show_change=true;
+    $('.page-content-bg').click(function(event) {
+        $('.left-nav').animate({left: '-221px'}, 100);
+        $('.page-content').animate({left: '0px'}, 100);
         $(this).hide();
-    })
+    });
 
-
-    //窄屏下的左侧菜单隐藏效果
-    $('.container .open-nav i').click(function(event) {
-        $('#side-nav').toggle(400);
-        // $('.wrapper .left-nav').toggle(400)
+    $('.layui-tab-close').click(function(event) {
+        $('.layui-tab-title li').eq(0).find('i').remove();
     });
 
     //左侧菜单效果
-    $('.left-nav #nav li').click(function () {
-        if($(this).hasClass('open')){
-            $(this).removeClass('open');
-            $(this).find('.nav_right').html('&#xe697;');
-            $(this).children('.sub-menu').slideUp();
-            // $(this).siblings().children('.sub-menu').slideUp();
+    // $('#content').bind("click",function(event){
+    $('.left-nav #nav li').click(function (event) {
+
+        if($(this).children('.sub-menu').length){
+            if($(this).hasClass('open')){
+                $(this).removeClass('open');
+                $(this).find('.nav_right').html('&#xe697;');
+                $(this).children('.sub-menu').stop().slideUp();
+                $(this).siblings().children('.sub-menu').slideUp();
+            }else{
+                $(this).addClass('open');
+                $(this).children('a').find('.nav_right').html('&#xe6a6;');
+                $(this).children('.sub-menu').stop().slideDown();
+                $(this).siblings().children('.sub-menu').stop().slideUp();
+                $(this).siblings().find('.nav_right').html('&#xe697;');
+                $(this).siblings().removeClass('open');
+            }
         }else{
-            $(this).addClass('open');
-            $(this).find('.nav_right').html('&#xe6a6;');
-            $(this).children('.sub-menu').slideDown();
-            $(this).siblings().children('.sub-menu').slideUp();
-            $(this).siblings().removeClass('open');
+
+            var url = $(this).children('a').attr('_href');
+            var title = $(this).find('cite').html();
+            var index  = $('.left-nav #nav li').index($(this));
+
+            for (var i = 0; i <$('.x-iframe').length; i++) {
+                if($('.x-iframe').eq(i).attr('tab-id')==index+1){
+                    tab.tabChange(index+1);
+                    event.stopPropagation();
+                    return;
+                }
+            };
+            
+            tab.tabAdd(title,url,index+1);
+            tab.tabChange(index+1);
         }
         
+        event.stopPropagation();
+         
     })
-
-    //初始化菜单展开样式
-    $('.left-nav #nav li .opened').siblings('a').find('.nav_right').html('&#xe6a6;');
-
-    
-
-
-
-
-
     
 })
 
@@ -154,7 +145,7 @@ function x_admin_show(title,url,w,h){
         url="404.html";
     };
     if (w == null || w == '') {
-        w=800;
+        w=($(window).width()*0.9);
     };
     if (h == null || h == '') {
         h=($(window).height() - 50);
@@ -176,3 +167,5 @@ function x_admin_close(){
     var index = parent.layer.getFrameIndex(window.name);
     parent.layer.close(index);
 }
+
+
